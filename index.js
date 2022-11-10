42ba9c138aa214fcb86316d03ec1eb7a19dd3aa4
whitebibs <115084982+whitebibs@users.noreply.github.com>
2022-11-10 16:32:15 +0100

    feat:search menu

---
+++ index.js
@@ -0,0 +1,22 @@
+const addClassById = (idButton, idTarget, classe) => {
+    let action = document.getElementById(idButton);
+    let target = document.getElementById(idTarget);
+    action.addEventListener("click",() =>
+    target.classList.toggle(classe));
+}
+
+function toggleStyle(el, styleName, value) {
+    if (el.style[styleName] === '') {
+        el.style[styleName] = value;
+    } else {
+        el.style[styleName] = '';
+    }
+}
+
+addClassById("search", "search-menu", "none");
+addClassById("button2", "search-menu", "none");
+
+for (let i = 1; i <= 4; i++) {
+    addClassById(`sub-menu-button-${i}`, `sub-menu-${i}`, "none");
+    addClassById(`sub-menu-button-${i}`, `rotate${i}`, "reverse");
+    }
